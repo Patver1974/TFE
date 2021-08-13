ff3af974d3c26c55bde25e4cbf6347eb08642166
Patrick <patver1974@gmail.com>
2021-08-13 20:00:39 +0200

    menugestionbsd install partiel

---
+++ app/src/main/java/com/example/tft_jeu/Ajouterlieux/ModifierLieux.java
@@ -0,0 +1,14 @@
+package com.example.tft_jeu.Ajouterlieux;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModifierLieux extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modifier_lieux);
+    }
+}
---
+++ app/src/main/java/com/example/tft_jeu/menugestionbsd/MenugestionBsd.java
@@ -0,0 +1,14 @@
+package com.example.tft_jeu.menugestionbsd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenugestionBsd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menugestion_bsd);
+    }
+}
