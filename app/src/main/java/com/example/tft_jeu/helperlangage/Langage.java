f7258322fdba218d4fbf4396179d6dcb228db0b8
Patrick <patver1974@gmail.com>
2021-08-23 11:19:01 +0200

    fix anim page initial

---
+++ app/src/main/java/com/example/tft_jeu/helperlangage/Langage.java
@@ -0,0 +1,14 @@
+package com.example.tft_jeu.helperlangage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Langage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_langage);
+    }
+}
---
+++ app/src/main/java/com/example/tft_jeu/swipStack/ActivitySwipStackImg.java
@@ -0,0 +1,14 @@
+package com.example.tft_jeu.swipStack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitySwipStackImg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swip_stack_img);
+    }
+}
